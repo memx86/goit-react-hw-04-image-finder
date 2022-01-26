9ae1d598e5af40eece86ab76ead3b67632d50b99
Yevhen Malyshko <memx86@gmail.com>
2022-01-26 17:26:20 +0200

    close modal functionality

--- src/App.js
+++ src/App.js
@@ -20,6 +20,9 @@ class App extends Component {
     const image = { src, alt };
     this.setState(() => ({ image, showModal: true }));
   };
+  closeModal = () => {
+    this.setState(() => ({ showModal: false }));
+  };
   render() {
     const { query, image, showModal } = this.state;
     const { src, alt } = image;
@@ -34,7 +37,7 @@ class App extends Component {
           <ImageGallery query={query} onImageClick={this.onImageOpen} />
         </Section>
         {showModal && (
-          <Modal>
+          <Modal closeModal={this.closeModal}>
             <img src={src} alt={alt} />
           </Modal>
         )}
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,17 +1,32 @@
+import { Component } from "react";
 import PropTypes from "prop-types";
 import s from "./Modal.module.css";
 
-function Modal(props) {
-  const children = props.children;
-  return (
-    <div className={s.backdrop}>
-      <div className={s.modal}>{children}</div>
-    </div>
-  );
+class Modal extends Component {
+  static propTypes = {
+    closeModal: PropTypes.func.isRequired,
+    children: PropTypes.node.isRequired,
+  };
+  componentDidMount() {
+    document.addEventListener("keydown", this.onEsc);
+  }
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.onEsc);
+  }
+  onEsc = (e) => {
+    if (e.code === "Escape") this.props.closeModal();
+  };
+  onBackdropClick = (e) => {
+    if (e.target === e.currentTarget) this.props.closeModal();
+  };
+  render() {
+    const { children } = this.props;
+    return (
+      <div className={s.backdrop} onClick={this.onBackdropClick}>
+        <div className={s.modal}>{children}</div>
+      </div>
+    );
+  }
 }
-
-Modal.propTypes = {
-  children: PropTypes.node.isRequired,
-};
 
 export default Modal;
