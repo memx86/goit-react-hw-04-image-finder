ad1e65e9545de9c5cf87a3ad6245effeb649d4c6
Yevhen Malyshko <memx86@gmail.com>
2022-01-28 17:55:29 +0200

    app

--- src/App.js
+++ src/App.js
@@ -1,51 +1,92 @@
-import { Component, Fragment } from "react";
+import { Fragment, useState } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
 import Search from "components/Search";
 import Gallery from "components/Gallery";
 import Modal from "components/Modal";
 
-class App extends Component {
-  state = {
-    query: "",
-    image: {},
-    showModal: false,
-  };
-  onSearch = (query) => this.setState(() => ({ query }));
-  onImageOpen = (e) => {
+function App() {
+  const [query, setQuery] = useState("");
+  const [image, setImage] = useState({});
+  const [showModal, setShowModal] = useState(false);
+
+  const onImageOpen = (e) => {
     const src = e.target.dataset.src;
     const alt = e.target.alt;
     const image = { src, alt };
-    this.setState(() => ({ image, showModal: true }));
+    setImage(image);
+    setShowModal(true);
   };
-  closeModal = () => {
-    this.setState(() => ({ showModal: false }));
+  const closeModal = () => {
+    setShowModal(false);
   };
-  render() {
-    const { query, image, showModal } = this.state;
-    const { src, alt } = image;
-    return (
-      <Fragment>
-        <Search onFormSubmit={this.onSearch} />
-        <Gallery query={query} onImageClick={this.onImageOpen} />
-        {showModal && (
-          <Modal closeModal={this.closeModal}>
-            <img src={src} alt={alt} />
-          </Modal>
-        )}
-        <ToastContainer
-          position="top-right"
-          autoClose={3000}
-          hideProgressBar={true}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
-      </Fragment>
-    );
-  }
+
+  const { src, alt } = image;
+  return (
+    <Fragment>
+      <Search onFormSubmit={setQuery} />
+      <Gallery query={query} onImageClick={onImageOpen} />
+      {showModal && (
+        <Modal closeModal={closeModal}>
+          <img src={src} alt={alt} />
+        </Modal>
+      )}
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </Fragment>
+  );
 }
+
+// class App extends Component {
+//   state = {
+//     query: "",
+//     image: {},
+//     showModal: false,
+//   };
+//   onSearch = (query) => this.setState(() => ({ query }));
+//   onImageOpen = (e) => {
+//     const src = e.target.dataset.src;
+//     const alt = e.target.alt;
+//     const image = { src, alt };
+//     this.setState(() => ({ image, showModal: true }));
+//   };
+//   closeModal = () => {
+//     this.setState(() => ({ showModal: false }));
+//   };
+//   render() {
+//     const { query, image, showModal } = this.state;
+//     const { src, alt } = image;
+//     return (
+//       <Fragment>
+//         <Search onFormSubmit={this.onSearch} />
+//         <Gallery query={query} onImageClick={this.onImageOpen} />
+//         {showModal && (
+//           <Modal closeModal={this.closeModal}>
+//             <img src={src} alt={alt} />
+//           </Modal>
+//         )}
+//         <ToastContainer
+//           position="top-right"
+//           autoClose={3000}
+//           hideProgressBar={true}
+//           newestOnTop={false}
+//           closeOnClick
+//           rtl={false}
+//           pauseOnFocusLoss
+//           draggable
+//           pauseOnHover
+//         />
+//       </Fragment>
+//     );
+//   }
+// }
 export default App;
