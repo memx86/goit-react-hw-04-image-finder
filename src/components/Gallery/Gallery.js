3eea082ebf99d03ae6790cb45a9e70e17854557d
Yevhen Malyshko <memx86@gmail.com>
2022-01-31 22:04:59 +0200

    magic string optimization

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -15,19 +15,32 @@ const initialState = {
   error: "",
   more: false,
 };
+const TYPE = {
+  STATUS: "status",
+  IMAGES: "images",
+  CLEAR: "clear",
+  ERROR: "error",
+  MORE: "more",
+};
+const STATUS = {
+  IDLE: "idle",
+  LOADING: "loading",
+  SUCCESS: "success",
+  ERROR: "error",
+};
 function reducer(state, action) {
   const { type, payload } = action;
   const { images } = state;
   switch (type) {
-    case "status":
+    case TYPE.STATUS:
       return { ...state, status: payload };
-    case "images":
+    case TYPE.IMAGES:
       return { ...state, images: [...images, ...payload] };
-    case "clear":
+    case TYPE.CLEAR:
       return { ...state, images: [] };
-    case "error":
+    case TYPE.ERROR:
       return { ...state, error: payload };
-    case "more":
+    case TYPE.MORE:
       return { ...state, more: payload };
     default:
       throw new Error(`Unsupported action type: ${type}`);
@@ -39,7 +52,7 @@ function Gallery({ query, onImageClick }) {
   const { status, images, error, more } = state;
   useEffect(() => {
     if (!query) return;
-    dispatch({ type: "clear" });
+    dispatch({ type: TYPE.CLEAR });
     pixabayApiService.resetPage();
     getImages(query);
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -47,10 +60,10 @@ function Gallery({ query, onImageClick }) {
 
   async function getImages(query) {
     if (error) {
-      dispatch({ type: "status", payload: "loading" });
-      dispatch({ type: "error", payload: "" });
+      dispatch({ type: TYPE.STATUS, payload: STATUS.LOADING });
+      dispatch({ type: TYPE.ERROR, payload: "" });
     }
-    dispatch({ type: "more", payload: true });
+    dispatch({ type: TYPE.MORE, payload: true });
     pixabayApiService.query = query;
     try {
       const response = await pixabayApiService.getImages();
@@ -58,14 +71,14 @@ function Gallery({ query, onImageClick }) {
     } catch (response) {
       onError(response);
     }
-    dispatch({ type: "more", payload: false });
+    dispatch({ type: TYPE.MORE, payload: false });
   }
   const onSuccess = (response) => {
     const totalHits = response.totalHits;
     if (!totalHits) {
       const error = "Can't find image";
-      dispatch({ type: "status", payload: "error" });
-      dispatch({ type: "error", payload: error });
+      dispatch({ type: TYPE.STATUS, payload: STATUS.ERROR });
+      dispatch({ type: TYPE.ERROR, payload: error });
       return;
     }
 
@@ -75,15 +88,15 @@ function Gallery({ query, onImageClick }) {
       const error =
         "We're sorry, but you've reached the end of search results.";
       toast.error(error);
-      dispatch({ type: "error", payload: error });
+      dispatch({ type: TYPE.ERROR, payload: error });
       return;
     }
 
     if (pixabayApiService.page === 2) {
       toast.success(`We found ${totalHits} images!`);
     }
-    dispatch({ type: "images", payload: newImages });
-    dispatch({ type: "status", payload: "success" });
+    dispatch({ type: TYPE.IMAGES, payload: newImages });
+    dispatch({ type: TYPE.STATUS, payload: STATUS.SUCCESS });
     if (!error) setTimeout(scrollCard, 500);
   };
   const onError = (error) => {
@@ -92,7 +105,7 @@ function Gallery({ query, onImageClick }) {
         ? "We're sorry, but you've reached the end of search results."
         : "Sorry, there is no response from server. Please try again.";
     toast.error(errorMsg);
-    dispatch({ type: "error", payload: errorMsg });
+    dispatch({ type: TYPE.ERROR, payload: errorMsg });
   };
   function scrollCard() {
     const { height: cardHeight } = document
@@ -105,11 +118,11 @@ function Gallery({ query, onImageClick }) {
     });
   }
 
-  if (status === "idle") return <div></div>;
+  if (status === STATUS.IDLE) return <div></div>;
 
-  if (status === "loading") return <Loader />;
+  if (status === STATUS.LOADING) return <Loader />;
 
-  if (status === "error") {
+  if (status === STATUS.ERROR) {
     return (
       <p className={s.error}>
         {error}: {query}
@@ -117,7 +130,7 @@ function Gallery({ query, onImageClick }) {
     );
   }
 
-  if (status === "success") {
+  if (status === STATUS.SUCCESS) {
     return (
       <Section>
         <ImageGallery images={images} onImageClick={onImageClick} />
